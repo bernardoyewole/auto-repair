0ec5a9620a9bfbab1085be8f54a03f5421580946
Bernard <oyebenard@gmail.com>
2024-07-22 18:59:01 -0500

    Add hero banner and header

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './assets/style/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
